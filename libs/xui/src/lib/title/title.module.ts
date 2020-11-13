3320e4c34814ddc0859cf528b18aea1f8a37cab9
Rene Cincura <rene@cincura.eu>
2020-11-13 22:50:45 +0100

    feat(angular): update to version 11

--- libs/xui/src/lib/title/title.module.ts
+++ libs/xui/src/lib/title/title.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { XuiTitleComponent } from './title.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatSelectModule, MatSlideToggleModule],
   declarations: [XuiTitleComponent],
 })
 export class TitleModule {}
