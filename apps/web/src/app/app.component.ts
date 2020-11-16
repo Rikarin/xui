1e25165451a89dc596e3ebf676a2de51bb8bcfaf
Rene Cincura <rene@cincura.eu>
2020-11-16 19:48:36 +0100

    fix(tests): fix tests

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'xui-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'web';
-  code = '<html>foobar</html>';
-}
+export class AppComponent {}
