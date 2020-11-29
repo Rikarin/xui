37b0f45a7bfe00d82581b90d8b44abecafe247ac
Rene Cincura <rene@cincura.eu>
2020-11-29 23:27:44 +0100

    fix(page): fix page

--- libs/xui/src/lib/button/button-group.component.ts
+++ libs/xui/src/lib/button/button-group.component.ts
@@ -11,15 +11,7 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
-  host: {
-    // '[class]': 'getStyle()',
-    // '[attr.disabled]': 'disabled || null',
-  },
 })
 export class XuiButtonGroupComponent {
-  @Input() xSize: 'sm' | 'md' | 'lg' = 'md';
-
-  // private getStyle() {
-  //   return `xui-button xui-button-${this.xSize} xui-button-${this.xType} xui-button-${this.xColor}`;
-  // }
+  // @Input() xSize: 'sm' | 'md' | 'lg' = 'md';
 }
