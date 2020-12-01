f138533b328706bc1e2d42c13fa648083aecfaf8
Rene Cincura <rene@cincura.eu>
2020-12-01 21:25:04 +0100

    feat(scrollbar): add custon scrollbar

--- libs/xui/src/lib/menu/menu.component.ts
+++ libs/xui/src/lib/menu/menu.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'ul[xui-menu]',
   exportAs: 'xuiMemu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class XuiMenuComponent implements OnInit {
   constructor() {}
