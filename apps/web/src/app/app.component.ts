480b33e963cb0aec686f15232c8340548a81f1d5
Rikarin <rene@cincura.eu>
2022-11-03 00:23:20 +0100

    feat: add light theme

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'xui';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    '[class]': 'getStyles()'
+  }
 })
-export class AppComponent {}
+export class AppComponent {
+  themePicker = new FormControl();
+
+  getStyles() {
+    return this.themePicker.value ? 'light-theme' : 'dark-theme';
+  }
+}
