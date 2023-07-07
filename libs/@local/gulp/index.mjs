0673cdfec1130eeb4274d3f195f83556d3e5e104
Rikarin <rene@cincura.eu>
2023-07-07 20:15:16 +0300

    refactor: fix build regarding previous refactorings

---
+++ libs/@local/gulp/index.mjs
@@ -0,0 +1,10 @@
+import gulp from 'gulp';
+
+
+export function projectTasks() {
+  // TODO: copy readme
+  // automatically set current year
+  // generate schematics for simplier usage
+  // update version in dist package.json
+  gulp.task('post-build', (done) => done());
+}
