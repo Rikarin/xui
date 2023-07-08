95f70c4a9550316cf0b06fdb9bfbd41deabd8223
Rikarin <rene@cincura.eu>
2023-07-09 02:17:09 +0300

    chore: add SSR and prerender support

---
+++ apps/web/src/app/app.server.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RootComponent } from './root.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
+import { environment } from '../environments/environment';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { ServerModule } from '@angular/platform-server';
+
+// TODO: fix this
+const routes: Routes = [
+  { path: 'theme-designer', loadChildren: () => import('./designer/designer.module').then(x => x.DesignerModule) },
+  { path: 'docs', loadChildren: () => import('./docs/docs.module').then(x => x.DocsModule) },
+  { path: '', redirectTo: '/docs', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [RootComponent],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    NgxGoogleAnalyticsModule.forRoot(environment.ga),
+    NgxGoogleAnalyticsRouterModule,
+
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    }),
+
+    TranslateModule.forRoot(),
+    ServerModule
+  ],
+  bootstrap: [RootComponent]
+})
+export class AppServerModule {
+  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer, translations: TranslateService) {
+    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:4200/assets/mdi.svg'));
+
+    translations.setDefaultLang('en-US');
+  }
+}
